bbb682b46a2c80f9d734b03a33665a40aedb6eef
diego-lan <122657977+diego-lan@users.noreply.github.com>
2023-05-16 22:00:19 -0300

    Create a 5 rounds game
    Here's two new functions. The first one at line 38
    has a roud counter that allows you to play 5 times
    and increases the value of playerScore or computerScore
    variables, depending on who has won the round.
    
    Then the theWinnerIs function, compares wich of playerScore
    or computerScore has the bigger number.
    
    Finally, on line 62, we console.log the result of
    theWinnerIs function, in other words, it tells us
    who's won.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const options = ['ROCK', 'PAPER', 'SCISSORS'];
 
+let playerScore = 0;
+let computerScore = 0;
+
 /* This function generates and return a random number between 0 and 1, multiplyes it by the number of the available options
 and round it down */
 function getComputerChoice() {
@@ -21,11 +24,39 @@ function playRound(player, computer) {
         (player == 'SCISSORS' && computer == 'PAPER') || 
         (player == 'PAPER' && computer == 'ROCK')) { 
             result = `You win! ${player} beats ${computer}`;
+            playerScore ++;
     } else {
         result = `You lose! ${computer} beats ${player}`;
+        computerScore ++;
     }
+    console.log(`You\'ve chosen ${player}`);
+    console.log(`Computer\'s choice is ${computer}`);
     return result;
 }
-   
-const playerSelection = prompt(`Enter Rock, Paper or Scissors.`).toUpperCase();
-const computerSelection = getComputerChoice();
+
+/* This function allows us to play 5 times in a row */
+function game() {
+    let counter = 0;
+    while (counter < 5) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt(`Enter Rock, Paper or Scissors.`).toUpperCase();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(''); // /this is just to leave an empty line to separate each round and make things more readable
+        counter ++;
+    }
+
+}
+
+/* This function anounces the winner */
+function theWinnerIs(playerScore, computerScore) {
+    if(playerScore < computerScore) {
+        return (`You win!`);
+    } else if (playerScore > computerScore) {
+        return (`You lose!`);
+    } else {
+        return (`It\'s a tie! Play again!`);
+    }
+}
+
+game();
+console.log(theWinnerIs());
